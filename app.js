e273aaba1ab066a3a0e24d04cf2d450f2eba76fb
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-03-19 11:40:16 +0900

    json 데이터 넘기기

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 // 루트 경로에 대한 GET 요청 처리
-app.get('/Hello', (req, res) => {
-  res.send('Hello, World!');
+app.post('/swag', (req, res) => {
+  // 클라이언트에게 응답 내용 전송
+  res.send(req.body);
 });
 
 // 서버를 3000번 포트에서 실행
 app.listen(3000, () => {
   console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
 });
-6
