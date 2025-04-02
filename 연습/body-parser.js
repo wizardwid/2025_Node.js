fa0adc380a4ae8fbde6c86733ab82c0aedddf346
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-04-02 14:09:27 +0900

    body-parser 신버전

--- 연습/body-parser.js
+++ 연습/body-parser.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/submit', (req, res) => {
     const {name, year} = req.body;  
