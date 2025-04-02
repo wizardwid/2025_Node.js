392afed448d0abbe7fd5fd0850f7073138a482fe
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-04-02 14:08:18 +0900

    body-parser 구버전

---
+++ 연습/body-parser.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit', (req, res) => {
+    const {name, year} = req.body;  
+    res.send(`Name : ${name}, Year: ${year}`);
+});
+
+// 서버를 3001번 포트에서 실행
+app.listen(3001, () => {
+    console.log('서버가 http://localhost:3001 에서 실행 중입니다.');
+});
